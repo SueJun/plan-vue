78d030f2a0ddc7bc7480b596440515b6fc4d2940
wujun <wujun@skyworth.com>
2022-10-09 09:29:44 +0800

    feat: 添加路由

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import {createRouter, RouteRecordRaw, Router, createWebHistory} from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/plan/index.vue'),
+        meta: {
+            title: '首页'
+        }
+    }
+]
+
+const router: Router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
+    },
+    extensions: [".ts", ".js", '.jsx', '.tsx'],
+  },
   plugins: [vue()]
 })
